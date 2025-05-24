c61f266061a7ea75314089c18938262795331659
JuanfranV <juanfranmendoza243@gmail.com>
2025-05-23 21:02:28 -0600

    segundo sin nada bueno solo se empezó a hacer pues todo

---
+++ src/main/java/org/example/dao/impl/categoriaDAOImpl.java
@@ -0,0 +1,27 @@
+package org.example.dao.impl;
+
+import org.example.dao.categoriaDAO;
+import org.example.model.categoriaModel;
+
+public class categoriaDAOImpl implements categoriaDAO {
+
+    @Override
+    public void crear(categoriaModel categoriaModel) {
+
+    }
+
+    @Override
+    public categoriaModel leer(int id) {
+        return null;
+    }
+
+    @Override
+    public void actualizar(categoriaModel categoriaModel) {
+
+    }
+
+    @Override
+    public void eliminar(int id) {
+
+    }
+}
---
+++ src/main/java/org/example/dao/impl/credencialDAOImpl.java
@@ -0,0 +1,27 @@
+package org.example.dao.impl;
+
+import org.example.dao.credencialDAO;
+import org.example.model.credencialModel;
+
+public class credencialDAOImpl implements credencialDAO {
+
+    @Override
+    public void crear(credencialModel credencialModel) {
+
+    }
+
+    @Override
+    public credencialModel leer(int id) {
+        return null;
+    }
+
+    @Override
+    public void actualizar(credencialModel credencialModel) {
+
+    }
+
+    @Override
+    public void eliminar(int id) {
+
+    }
+}
